dc8db25f1fd1ccad846d13afd6d3ef5847c9e7ba
сергей слиж <sergeislizh@mail.ru>
2017-03-12 23:15:38 +0300

    //solution

---
+++ SlipSort/SortIntArr.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlipSort
+{
+
+    /// <summary>  
+    ///  This class is intended for array sorting
+    /// </summary>  
+    public class SortIntArr
+    {
+        /// <summary>  
+        ///  This method sorts to arrays into another one by merge sort
+        /// </summary>  
+        static int[] SlipSort(int[] leftArr, int[] rightArr)
+        {
+            int[] arr = new int[leftArr.Length + rightArr.Length];
+            int i, iLeft, iRight;
+            i = iLeft = iRight = 0;
+            while (iLeft != leftArr.Length && iRight != rightArr.Length)
+            {
+                if (leftArr[iLeft] <= rightArr[iRight])
+                    arr[i++] = leftArr[iLeft++];
+                else arr[i++] = rightArr[iRight++];
+            }
+            while (iLeft != leftArr.Length)
+                arr[i++] = leftArr[iLeft++];
+            while (iRight != rightArr.Length)
+                arr[i++] = rightArr[iRight++];
+            return arr;
+        }
+        /// <summary>  
+        ///  This method divides array into two parts recursively until their sizes are two or one
+        /// </summary>  
+        public static int[] Sort(int[] arr)
+        {
+            if (arr.Length == 1) return arr;
+            if (arr.Length == 2)
+            {
+                if (arr[1] < arr[0])
+                {
+                    int buf = arr[1];
+                    arr[1] = arr[0];
+                    arr[0] = buf;
+                }
+                return arr;
+            }
+            int midle = (arr.Length - 1) / 2;
+            int[] leftArr = new int[midle + 1];
+            int[] rightArr = new int[arr.Length - midle - 1];
+            for (int i = 0; i < leftArr.Length; i++)
+                leftArr[i] = arr[i];
+            for (int i = 0; i < rightArr.Length; i++)
+                rightArr[i] = arr[i + midle + 1];
+            leftArr = Sort(leftArr);
+            rightArr = Sort(rightArr);
+            arr = SlipSort(leftArr, rightArr);
+            return arr;
+        }
+    }
+}
---
+++ TestingSlipSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlipSort;
+
+namespace TestingSlipSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 12, -4, 5, 9, 10, 3, 5, -7, -14, 9 };
+            arr = SortIntArr.Sort(arr);
+            foreach (int el in arr)
+                Console.Write(el + " ");
+            Console.WriteLine();
+        }
+    }
+}
